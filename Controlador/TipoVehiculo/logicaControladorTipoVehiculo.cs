17419f3f834c23bd3dcb218b53ca039687ae5d75
Josesanchez152100 <70731465+Josesanchez152100@users.noreply.github.com>
2020-11-24 22:21:27 -0500

    Initial commit

---
+++ Controlador/TipoVehiculo/logicaControladorTipoVehiculo.cs
@@ -0,0 +1,22 @@
+﻿using Modelo.TipoVehiculo;
+using System.Data;
+
+namespace Controlador.TipoVehiculo
+{
+    public class logicaControladorTipoVehiculo
+    {
+        //Negociar INSERT
+        public int NegociarInsertTipoVehiculo(int id, string name)
+        {
+            AccesoMetodosCRUDtipoVehiculo negociarAcceso = new AccesoMetodosCRUDtipoVehiculo();
+
+            return negociarAcceso.InsertTipoVehiculo(id, name);
+        }
+
+        //Negociar SELECT
+        public static DataTable NegociarSelectTipoVehiculo()
+        {
+            return AccesoMetodosCRUDtipoVehiculo.ListTipoVehiculo();
+        }
+    }
+}
---
+++ Modelo/TipoVehiculo/AccesoMetodosCRUDtipoVehiculo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.TipoVehiculo
+{
+    public class AccesoMetodosCRUDtipoVehiculo
+    {
+        //Crear la operación INSERT
+        public int InsertTipoVehiculo(int id, string nombre)
+        {
+            SqlCommand _comando = MetodosCRUDtipoVehiculo.CrearComandoProcAlmacInsert_tv();
+
+            _comando.Parameters.AddWithValue("@id", id);
+            _comando.Parameters.AddWithValue("@nombre", nombre);
+
+            return MetodosCRUDtipoVehiculo.EjecutarComandoProcAlmacInsert_tv(_comando);
+        }
+
+        //Operacion SELECT
+        public static DataTable ListTipoVehiculo() 
+        {
+            SqlCommand _comando = MetodosCRUDtipoVehiculo.CrearComandoSelect_tv();
+
+            _comando.CommandText = "select * from Tipo_Vehiculo";
+
+            return MetodosCRUDtipoVehiculo.EjecutarComandoSelect_tv(_comando);
+        }
+    }
+}
